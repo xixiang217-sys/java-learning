08a9ec70fd3a2d90928ec43c3a3aca608f924ce0
xixiang217-sys <xixiang217@gmail.com>
2026-06-11 20:16:37 +0800

    opppractice

---
+++ oop/src/opptest6/Student.java
@@ -0,0 +1,13 @@
+package opptest6;
+
+public class Student {
+    private int age;
+
+    public void setAge(int age){
+        this.age = age;
+        age=9;
+        System.out.println(this.age);
+        System.out.println(age);
+    }
+
+}
---
+++ oop/src/opptest6/test.java
@@ -0,0 +1,8 @@
+package opptest6;
+
+public class test {
+   public static void main(String[] args) {
+     Student s1=new Student();
+     s1.setAge(8);
+   }
+}
