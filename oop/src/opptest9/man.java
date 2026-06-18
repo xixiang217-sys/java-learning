a6884e8e0b303bfb993cc5d6ce4b9137f3a5ef1c
xixiang217-sys <xixiang217@gmail.com>
2026-06-18 22:01:51 +0800

    opp

--- oop/src/opptest9/man.java
+++ oop/src/opptest9/man.java
@@ -1,4 +1,12 @@
 package opptest9;
 
 public class man {
-}
+
+    int age;
+         String name;
+      static String    Nmae;
+
+
+        }
+
+
---
+++ oop/src/opptest9/test.java
@@ -0,0 +1,19 @@
+package opptest9;
+
+public class test {
+    public static void main(String[] args) {
+        man m1=new man();
+        m1.age=18;
+        m1.name="张三";
+        man.Nmae="小小";
+
+        man m2=new man();
+        m2.age=19;
+        m2.name="张5";
+
+
+        System.out.println(m1.age+m1.name+m1.Nmae);
+        System.out.println(m2.age+m2.name+m2.Nmae);
+    }
+
+}
