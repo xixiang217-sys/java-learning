8c21c8cfaef9906378ee59ffeeefaa2ad515c958
xixiang217-sys <xixiang217@gmail.com>
2026-06-24 22:25:19 +0800

    final

---
+++ oop/src/opptest11/opptest11.java
@@ -0,0 +1,22 @@
+package opptest11;
+
+public class opptest11 {
+
+        private double r;
+        final double pi=3.14;
+        //空参构造
+        public opptest11(){
+        }
+        public opptest11(double r){
+            this.r=r;
+        }
+        public double getR(){
+            return r;
+        }
+        public void setR(double r){
+            this.r=r;
+
+    }
+
+
+}
---
+++ oop/src/opptest11/test.java
@@ -0,0 +1,13 @@
+package opptest11;
+
+import java.util.Set;
+
+public class test {
+    public static void main(String[] args) {
+       opptest11 y=new opptest11();
+        y.setR(10);
+
+        System.out.println(y.getR()*y.getR()*y.pi);
+
+    }
+}
